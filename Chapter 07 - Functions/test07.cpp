f5660bce0005f14139657a7c43187ed16c5d234d
Nick <wolfornd@miamioh.edu>
2017-12-19 23:13:35 -0500

    int and filter_int reading mostly done, currently some undefined behavior related to console input, though it does work.

--- Chapter 07 - Functions/test07.cpp
+++ Chapter 07 - Functions/test07.cpp
@@ -6,18 +6,66 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <limits>
 
-//apparently its REQUIRED that you declare the functions up front, didn't realize that from the example
 std::vector<double> read_double_vector();
+int get_filter_width();
+int read_int();
 
+//It should be noted that std::cin and std::cout do not play nice together while on the same line
+//when they are called they can flush data that was waiting to be printed.
 int main(){
-	std::cout << "Enter data signal as space delimited doubles\n";
-	std::vector<double> v = read_double_vector();
-	for(double d : v){
-		std::cout << d << " ";
-	}
+	//std::cout << "Enter data signal as space delimited doubles\n";
+	//std::vector<double> v = read_double_vector();
+	
+	std::cout << read_int();
 	std::cout << "\n";
+	std::cout << get_filter_width();
+	std::cout << "\n";
+	//int width = get_filter_width();
+
+	
 	return 0;
+}
+
+//gets the width of the filter to be applied
+//rounds up to the nearest positive, odd int, greater than 1 (a moving filter of width 1 is redundant)
+int get_filter_width(){
+	std::cin.clear();
+	std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+	std::cout << "enter the int width of the filter (should be odd and positive)\n";
+	int w = read_int();
+	if(w < 3){
+		w = 3;
+	}else if(w % 2 == 0){
+		w++;
+	}
+	
+	return w;
+}
+
+//probably going to have to read ints in the future as well so might as well not duplicate writing this as well
+//has the problem that if the user has issues entering a number, will continuously expect one
+//given the scope of these problems, I don't think further error checking/user friendliness is needed
+int read_int(){
+	std::cout << "enter int\n";
+	int num;
+	std::cin.clear();
+	std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+	while(true){
+		if(std::cin >> num){
+			
+			return num;
+		}else{
+			//tell the user more input is expected and clear the error;
+			std::cout << "that was not a valid input, try again";
+			std::cin.clear();
+			std::string s;
+			std::getline(std::cin,s);
+		}
+
+	}
+	
 }
 
 //I thought the way the example was set up was a litte bit messy
