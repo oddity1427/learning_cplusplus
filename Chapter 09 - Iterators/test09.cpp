09b4cdbdae7611634cae2039c883d33e1944b740
Nick <wolfornd@miamioh.edu>
2018-01-08 20:48:56 -0500

    test09.cpp complete

--- Chapter 09 - Iterators/test09.cpp
+++ Chapter 09 - Iterators/test09.cpp
@@ -2,3 +2,15 @@
 //will be implementing a slightly smarter merge sort, taking advantage of natural runs
 //Not going to be implementing any sort of searching, I figure this will be more than enough to get iterators down.
 
+#include "vector_algos.hpp"
+#include <iostream>
+
+int main(){
+	std::vector<double> input = read_double_vector(std::string s = "Input space delimited doubles, ctrl+d to exit in terminal");
+	std::cout << "mergesort used to sort input, displayed sorted below";
+	d = mergesort(d);
+	for(double d : input){
+		std::cout << d << " ";
+	}
+	std::cout << "\n";
+}
